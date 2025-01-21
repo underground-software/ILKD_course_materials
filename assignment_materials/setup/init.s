66d3049e92989f90b76836ed251df1bc793859b9
Joel Savitz <jsavitz@redhat.com>
2025-01-21 01:33:29 -0500

    setup: init.s: fix incorrect comment format and other doc changes
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>

--- assignment_materials/setup/init.s
+++ assignment_materials/setup/init.s
@@ -5,18 +5,18 @@ _start:
 	la a1, .Lmsg
 	lui a2, %hi(.Lmsglen)
 	addi a2, a2, %lo(.Lmsglen)
-	li a7, 64	#nr_write
+	li a7, 64	  # nr_write
 	ecall
 	li a0, 0
 	la a1, .Lbuf
 	lui a2, %hi(.Lbuflen)
 	addi a2, a2, %lo(.Lbuflen)
-	li a7, 63	#nr_read
+	li a7, 63	  # nr_read
 	ecall
-	li a0, 0xfee1dead
-	li a1, 0x28121969
-	li a2, 0x4321fedc //CMD_HALT
-	li a7, 142	#nr_rebbot
+	li a0, 0xfee1dead # "feel dead"
+	li a1, 0x28121969 # Linus Torvald's birthday
+	li a2, 0x4321fedc # CMD_HALT
+	li a7, 142	  # nr_rebbot
 	ecall
 	unimp
 .data
